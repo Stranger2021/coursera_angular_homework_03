13bc0e3d67c27b81f967709e5913a66f96363dc8
Stranger2021 <segat1981@gmail.com>
2021-03-09 15:33:24 +0100

    Homework 03. Version 3.0

--- app.js
+++ app.js
@@ -5,30 +5,19 @@ angular.module('NarrowItDownApp', [])
 .controller('NarrowItDownController', NarrowItDownController)
 .service('MenuSearchService', MenuSearchService)
 .constant('PathApi', "https://davids-restaurant.herokuapp.com")
-.directive('foundItems', FoundItems)
-.directive('myFoundItems', MyFoundItems);
-
+.directive('foundItems', FoundItems);
 
 // Пользовательская директива (для реализации шаблона)
 function FoundItems() {
   var ddo = {
-    templateUrl: "menuitem.html" // Шаблон, который отвечает за директиву
-  }
-
-  return ddo;
-}
-
-// Пользовательская директива (свойство директивы)
-function MyFoundItems() {
-  var ddo = {
+    templateUrl: "menuitem.html", // Шаблон, который отвечает за директиву
+    //restrict: 'AE',
     scope: {
-      menu: '<myFoundItems',       // Свойство директивы, которое передает список отфильтрованных записей
-      title: '@'
-    },
-    controller: NarrowItDownController,
-    controllerAs: 'menu',
-    bindToController: true
-  }
+      myFoundItems: '<',    // слева - как использовать в директиве, справа - имя атрибута
+      onRemove: '&',
+      title: '<'
+    }
+  };
 
   return ddo;
 }
@@ -40,69 +29,38 @@ NarrowItDownController.$inject = ['MenuSearchService'];
 function NarrowItDownController(MenuSearchService) {
   var menu = this;
 
-  menu.foundItems = [];
   menu.searchText = "";
-  menu.title = "Nothing found";
 
- // Функция для поиска вхождения текста в свойства элементов массива
- function MyInclude(elementOfList) {
-   var strNew = menu.searchText.toLowerCase();
+  // Генерация заголовка таблицы с указание кол-ва элементов
+  menu.title = function() {
+    return MenuSearchService.UpdateTitle();
+  }
 
-   if ((elementOfList.short_name != null)         && (elementOfList.short_name.toLowerCase().includes(strNew)) )          { return true; };
-   if ((elementOfList.name != null)               && (elementOfList.name.toLowerCase().includes(strNew)) )                { return true; };
-   if ((elementOfList.description != null)        && (elementOfList.description.toLowerCase().includes(strNew)) )         { return true; };
-   if ((elementOfList.small_portion_name != null) && (elementOfList.small_portion_name.toLowerCase().includes(strNew)) )  { return true; };
-   if ((elementOfList.large_portion_name != null) && (elementOfList.large_portion_name.toLowerCase().includes(strNew)) )  { return true; };
- }
+  // Наличие элементов в списке
+  menu.IsFindItems = function() {
+    return MenuSearchService.IsFindItems();
+  }
 
- // Найдены ли записи?
- menu.IsFindItems = function () {
-    return (menu.foundItems.length > 0);
- };
-
- // Обновление заголовка списка
- function UpdateTilte() {
-   if (menu.IsFindItems()) {
-     menu.title = "List of Menu (" + menu.foundItems.length +")";
-   } else {
-     menu.title = "Nothing found";
-   };
- }
-
-  // Нажатие на кнопку "Narrow It Down For Me!"
+  // Нажатие на кнопку "Narrow It Down For Me!" - поиск записей в соответствии с фильтром
   menu.searchItems = function() {
 
-    menu.foundItems = [];
+      // Получение данных с помощью асинхронного сервиса http
+      var promise = MenuSearchService.getAllMenuItems();
 
-    // Ожидание ответа от сервера (получение итогового массива данных)
-    var promise = MenuSearchService.getMatchedMenuItems();
-
-    promise.then(function (response) {
-      // При успешном ответе, анализируем элементы и формируем новый массив
-      menu.items = response.data.menu_items;
-
-      // Поиск нужных записей и сохранение их в массив foundItems
-      if (menu.searchText != "") {
-        for (var i=0; i<menu.items.length; i++) {
-          if (MyInclude(menu.items[i]) == true) {
-            menu.foundItems.push(menu.items[i]);
-          }
-        }
-      }
-
-      // Изменение названия списка
-     UpdateTilte();
-    })
-    .catch(function (error) {
-      console.log("Something went terribly wrong.");
-    });
+      promise
+      .then( function(response) {
+          // Удачный ответ с сервера
+          menu.foundItems = MenuSearchService.getMatchedMenuItems(response, menu.searchText);
+      })
+      .catch(function(error) {
+          // Неудачный ответ с сервера
+          console.log("Error: Get data from server");
+      });
   };
-
 
   // Удаление элемента из списка
   menu.removeItem = function (itemIndex) {
-    menu.foundItems.splice(itemIndex, 1);
-    UpdateTilte();
+    menu.foundItems = MenuSearchService.removeItem(itemIndex);
   };
 }
 
@@ -113,32 +71,70 @@ MenuSearchService.$inject = ['$http', 'PathApi'];
 function MenuSearchService($http, PathApi) {
   var service = this;
 
-  service.getMatchedMenuItems = function () {
+  // Итоговый массив данных
+  var foundItems = [];
+
+  // Функция для поиска вхождения текста в свойства элементов массива
+  function MyInclude(elementOfList, searchTerm) {
+    var strNew = searchTerm.toLowerCase();
+
+    if ((elementOfList.short_name != null)         && (elementOfList.short_name.toLowerCase().includes(strNew)) )          { return true; };
+    if ((elementOfList.name != null)               && (elementOfList.name.toLowerCase().includes(strNew)) )                { return true; };
+    if ((elementOfList.description != null)        && (elementOfList.description.toLowerCase().includes(strNew)) )         { return true; };
+    if ((elementOfList.small_portion_name != null) && (elementOfList.small_portion_name.toLowerCase().includes(strNew)) )  { return true; };
+    if ((elementOfList.large_portion_name != null) && (elementOfList.large_portion_name.toLowerCase().includes(strNew)) )  { return true; };
+  }
+
+  // Наличие записей в массиве
+  service.IsFindItems = function () {
+    return (foundItems.length > 0);
+  }
+
+  //Обновление заголовка списка
+  service.UpdateTitle = function() {
+    if (service.IsFindItems()) {
+      return "List of Menu (" + foundItems.length +")";
+    } else {
+      return "Nothing found";
+    };
+  }
+
+  // Функция для подключения к серверу и загрузки данных о пунктах меню
+  service.getAllMenuItems = function () {
     var response = $http({
       method: "GET",
       url: (PathApi + "/menu_items.json")
-      // params: {
-      //   category: shortName
-      // }
     });
 
     return response;
+  }
+
+  // Получение отфильтрованного списка с сервера
+  service.getMatchedMenuItems = function (response, searchTerm) {
+
+    // При успешном ответе, анализируем элементы и формируем новый массив
+    var items = response.data.menu_items;
+
+    // Поиск нужных записей и сохранение их в массив foundItems
+    if (searchTerm != "") {
+      for (var i=0; i<items.length; i++) {
+        if (MyInclude(items[i], searchTerm) == true) {
+          foundItems.push(items[i]);
+        }
+      }
+    };
+
+    // Возвращаем массив отфильтрованных записей
+    //console.log("Найдено записей: " + foundItems.length);
+    return foundItems;
+  }
+
+  //Удаление элемента из списка
+  service.removeItem = function (itemIndex) {
+    foundItems.splice(itemIndex, 1);
+    return foundItems;
   };
+
 }
 
 })();
-
-
-
-
-
-
-// Объявите и создайте MenuSearchService. 
-// Сервис должен иметь следующий метод: getMatchedMenuItems(searchTerm). 
-// Этот метод будет отвечать за обращение к серверу (используя службу $http)
-// для получения списка всех пунктов меню. Как только он получит все пункты меню,
-// он должен перебрать их, чтобы выбрать те, чье описание соответствует поисковому термину. 
-// Как только список найденных элементов будет скомпилирован, он должен вернуть
-// этот список (завернутый в обещание). 
-//
-// Помните, что сама then-функция возвращает обещание. Ваш метод примерно будет выглядеть так:
